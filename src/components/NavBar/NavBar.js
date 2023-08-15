986c5000bfcdaeb21564acd34004791a2825b5cf
Daria Dorna <perelka4@onet.pl>
2023-08-15 15:35:54 +0200

    Add NavBaR

---
+++ src/components/NavBar/NavBar.js
@@ -0,0 +1,14 @@
+import styles from './NavBar.module.scss';
+
+const NavBar = () => {
+  return (
+    <nav className={styles.navbar}>
+      <a href="/"><span className="fa fa-bars"></span></a>
+      <span className={styles.navtxt}><a href="/">Home</a>
+        <a href="about">About</a>
+        <a href="favourite">Favourite</a>
+      </span>
+    </nav>
+  );
+}
+export default NavBar;
